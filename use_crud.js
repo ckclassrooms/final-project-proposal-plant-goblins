5509749aacb3cc21a153a15859fb6020cd38d186
Kenan Arica <kenanarica@Kenans-MacBook-Air-2.local>
2022-11-13 14:12:01 -0600

    adding CRUD functions

---
+++ use_crud.js
@@ -0,0 +1,13 @@
+import  { addPlant, getPlantsByUser, getWateringsByPlant, addWatering } from "./crud_ops.js"
+
+// addPlant("gred", 2)
+
+// let z = await getPlantsByUser(2)
+
+// console.log(z);
+
+console.log(await getWateringsByPlant(1));
+
+// console.log(addWatering(1, 5))
+
+
