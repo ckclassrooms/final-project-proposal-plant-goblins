d91dfc116c98442ec75c539b53c75dd3a29421e5
Kenan Arica <kenanarica@Kenans-MacBook-Air-2.local>
2022-11-29 20:00:09 -0600

    added waterings functionality

--- src/App.js
+++ src/App.js
@@ -4,65 +4,37 @@ import { supabase, getPlantsByUser, addWatering, getWateringsByPlant, getPlantIn
 import React, {useState, useEffect, componentDidMount} from 'react'
 import { render } from '@testing-library/react';
 
-// getPlantsByUser(5).then(response => {
-//   console.log(response)
-// })
-
-// THIS KINDA WORKS BUT NOT REALLY. REDO THIS CODE FROM SCRATCH.
-
-// class PlantList extends React.Component { 
-//   constructor(props) { 
-//     super(props)
-//     this.state = { 'all_plants' : [{'name' : 'PLANTS NOW LOADING'}]}
-//   }
-  
-//   componentDidMount() {
-//     getPlantsByUser(4).then(response => {
-//       this.setState({'all_plants' : response["data"]})
-//     });
-//   }
-//    render() {
-//     console.log("plantList render")
-    
-//     let all_plants = this.state.all_plants
-//     let plantList = []
-
-//     for(let i = 0; i < all_plants.length; i++) { 
-
-//       let plant_div_id = `plant_${all_plants[i]["id"]}`
-//       function handleButton(stuff) { 
-//         document.getElementById(plant_div_id).style.backgroundColor = "green";
-//       }
-
-//       console.log(all_plants[i])  
-//       plantList.push(
-
-//       <div class="plantEntry" id={plant_div_id}>
-//       <h3>{all_plants[i]["name"]}</h3>
-//       <button onClick={handleButton}>Water</button>    
-//       </div>
-//       )
-  
-//     }
-//     return <div class="plantContainer">
-//       <ul>{plantList}</ul>
-//     </div>
-//   }
-// }
 
 
-function PlantList({plants, setPlants, user}) { 
+
+// TODO: 
+// - deploy current build to netlify and test
+// - push to github
+// 
+// Change plant label based on last watered
+// if watered within a day, color green
+// if watered 1-3 days ago, yellow
+// if watered less than that, red
+// if never watered, blue
+
+function PlantList({plants, setPlants, user, getAllPlants}) { 
     let all_plants = plants  
     let plantList = []
+
     for(let i = 0; i < all_plants.length; i++) { 
 
       let plant_div_id = `plant_${all_plants[i]["id"]}`
       function handleButton(stuff) { 
         document.getElementById(plant_div_id).style.backgroundColor = "green";
       }
-      
+
       async function deletePlant() { 
         
+        let f = await supabase
+        .from('waterings')
+        .delete()
+        .eq('plant_id', all_plants[i]["id"])
+
         let z = await supabase
         .from('plants')
         .delete()
@@ -73,23 +45,33 @@ function PlantList({plants, setPlants, user}) {
           console.log(z["data"])
 
         } else { 
-          let resp = await supabase
-          .from('plants')
-          .select('*')
-          .eq('owner', user.id)
-          console.log(resp);
-          setPlants(resp["data"])
+
+          setPlants(await getAllPlants())
+
         }
 
       }
 
-      console.log(all_plants[i])  
+      async function waterPlant() { 
+        let resp = await supabase
+          .from('waterings')
+          .insert([{'plant_id' :  all_plants[i]["id"], 'observed_health' : 5}])
+          if(resp["error"]) { 
+            alert('Could not water this plant. Check the console for more details. Sorry!')
+            console.log(resp)
+          } 
+        }
+      console.log(all_plants[i])
       plantList.push(
 
       <div class="plantEntry" id={plant_div_id}>
       <h3>{all_plants[i]["name"]}</h3>
-      <button onClick={handleButton}>Water</button>  
-      <button onClick={deletePlant}>Delete</button>    
+      <button onClick={waterPlant}>Water</button>  
+      <button onClick={deletePlant}>Delete</button>  
+      <details>
+  <summary>Waterings</summary>
+  This text will be hidden if your browser supports it.
+</details>
       </div>
       )
   
@@ -98,7 +80,6 @@ function PlantList({plants, setPlants, user}) {
       <ul>{plantList}</ul>
     </div>
   }
-
 
 function App () {
 
@@ -119,11 +100,26 @@ function App () {
       }
       
     })
+
+    async function getAllPlants() { 
+      let resp = await supabase
+          .from('plants')
+          .select('*')
+          .eq('owner', user.id)
+          console.log(resp);
+          
+          for(let plant of resp["data"]) {
+            let waterings = await getWaterings(plant["id"])
+            plant["waterings"] = waterings
+          }
+          return(resp["data"])
+
+    }
+
     async function checkUser() {
       /* if a user is signed in, update local state */
       const user = await supabase.auth.getUser();
       setUser(user["data"]["user"]);
-      console.log("IN THIS ")
       let userid = user["data"]["user"]["id"]
       // get the user's plants 
       let resp = await supabase
@@ -135,6 +131,12 @@ function App () {
         alert('There was an issue getting your plants. Please check the console for more information. Sorry about that!')
         console.log(resp["error"])
       } else { 
+
+        // TODO: get waterings for each plant
+        for(let plant of resp["data"]) {
+          let waterings = await getWaterings(plant["id"])
+          plant["waterings"] = waterings
+        }
         setPlants(resp["data"])
       }
     }
@@ -171,14 +173,21 @@ function App () {
         console.log(z["error"])
       } else { 
 
-        let resp = await supabase
-        .from('plants')
-        .select('*')
-        .eq('owner', user.id)
-        console.log(resp);
-        setPlants(resp["data"])
+
+        console.log("WATERINGS:");
+        
+        // TODO: add in function call to getPlants
+        setPlants(await getAllPlants())
 
       }
+    }
+
+    async function getWaterings(plant_id) { 
+      let resp = await supabase
+      .from('waterings')
+      .select('*')
+      .eq('plant_id', plant_id)
+      return resp["data"]
     }
 
     console.log("CURRENT USER")
@@ -189,7 +198,7 @@ function App () {
           <h3>User: {user.email}</h3>
           <button onClick={signOut}>Sign out</button>
           <button onClick={addPlantF}>Add plant</button>
-                <PlantList plants={plants} setPlants={setPlants} user={user}/>
+                <PlantList plants={plants} setPlants={setPlants} user={user} getAllPlants={getAllPlants}/>
         </div>
       )
     }
